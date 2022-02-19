350e07d5cb5884ead7a3af738c089014fe8f9bf0
unknown <yeuthuongvoivangdiqua@gmail.com>
2022-02-19 16:36:14 +0700

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import model.Product;
+import service.Note;
 import service.ProductService;
 
 import java.util.Scanner;
@@ -6,23 +7,16 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         ProductService productService = new ProductService();
-        productService.addProduct(new Product(1, "Mũ bảo hiểm giá rẻ", 2000, "china"));
-        productService.addProduct(new Product(2, "Bàn chải đánh răng", 1000, "china"));
+        productService.addProduct(new Product(1, "Mũ bảo hiểm", 2000, "china"));
+        productService.addProduct(new Product(2, "Bàn chải", 1000, "china"));
         productService.addProduct(new Product(3, "Tăm", 3000, "Mexico"));
         productService.addProduct(new Product(4, "Dây điện", 13000, "America"));
+        productService.addProduct(new Product(5, "Bàn là", 6000, "America"));
 
         int choice = -1;
         while (choice != 0) {
             Scanner scanner = new Scanner(System.in);
-            System.out.println("Mục lục: ");
-            System.out.println("1. In danh sách sản phẩm có sẵn.");
-            System.out.println("2. Xóa phần tử trong danh sách.");
-            System.out.println("3. Tìm kiếm sản phẩm theo id.");
-            System.out.println("4. Sắp xếp sản phẩm theo giá tăng dần.");
-            System.out.println("5. Sắp xếp sản phẩm theo giá giảm dần.");
-            System.out.println("6. Sắp xếp sản phẩm theo tên.");
-            System.out.println("7. Thêm 1 sản phẩm.");
-            System.out.println("0. Kết thúc chương trình.");
+            Note.alertMenu();
             try {
                 choice = scanner.nextInt();
                 switch (choice) {
@@ -56,15 +50,13 @@ public class Main {
                         break;
                     case 0:
                         System.exit(0);
+                        break;
                     default:
-                        System.out.println("/************************************/");
                         System.out.println(choice + " không có trong mục lục mời nhập lại");
-
-                        System.out.println("=================  ==================");
-                        System.out.println("=====================================");
+                        break;
                 }
-            } catch (Exception e) {
-                System.out.println("Lỗi");
+            } finally {
+                System.out.println("Xong");
             }
         }
     }
---
+++ src/service/Note.java
@@ -0,0 +1,16 @@
+package service;
+
+public class Note {
+    public static void alertMenu() {
+        System.out.println("Mục lục: ");
+        System.out.println("1. In danh sách sản phẩm có sẵn.");
+        System.out.println("2. Xóa phần tử trong danh sách.");
+        System.out.println("3. Tìm kiếm sản phẩm theo id.");
+        System.out.println("4. Sắp xếp sản phẩm theo giá giảm dần .");
+        System.out.println("5. Sắp xếp sản phẩm theo giá tăng dần.");
+        System.out.println("6. Sắp xếp sản phẩm theo tên.");
+        System.out.println("7. Thêm 1 sản phẩm.");
+        System.out.println("0. Kết thúc chương trình.");
+    }
+    public static void alertDefault() {}
+}
